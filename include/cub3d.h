dbb36a1b3c487d448638f01a6ebd62822c03a7fa
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-03-20 19:26:48 +0100

    created structure for mlx and exit functions hook

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,14 +6,16 @@
 /*   By: glacroix <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 18:11:28 by glacroix          #+#    #+#             */
-/*   Updated: 2024/03/18 18:42:46 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:14:36 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 #define CUB3D_H
 
-#include "../libft/llibft.h"
+
+/*------------------------------Libraries-------------------------------------*/
+#include "../libft/libft.h"
 #include <mlx.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -22,6 +24,36 @@
 #include <sys/uio.h>
 #include <fcntl.h>
 
+/*------------------------------Colors----------------------------------------*/
+# define PURPLE			0XF4ECF7 
+# define ORANGE			0XFFDE5C
+# define RED			0X42990010
+# define GREEN			0X014421 
+# define BLACK			0X000000
+# define WHITE			0XFFF5EE
+# define BLUE			0X191970
 
+/*------------------------------Shortcuts-------------------------------------*/
+# define ESC				53
+# define W					13
+# define S					1
+# define A					0 
+# define D					2
+# define UP					126
+# define DOWN				125
+# define LEFT				123
+# define RIGHT				124
+# define PLUS				69
+# define MINUS				78
+
+typedef struct	s_data {
+	void	*img;
+	char	*img_addr;
+	int		img_bits_per_pixel;
+	int		img_line_length;
+	int		img_endian;
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,65 @@
 /*   By: glacroix <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 16:26:32 by glacroix          #+#    #+#             */
-/*   Updated: 2024/03/18 16:39:00 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/03/20 19:25:00 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
-#include "unistd.h"
+#include "cub3d.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = data->img_addr + (y * data->img_line_length + x * (data->img_bits_per_pixel / 8));
+	*(unsigned int*)pixel = color;
+}
+
+
+int ft_exit()
+{
+	ft_putstr_fd("Exited 3D, ggboiiii\n", 1);
+	exit(EXIT_SUCCESS);
+}
+
+int hook_key(int keycode)
+{
+	printf("keycode = %d\n", keycode);
+	if (keycode == ESC)
+		ft_exit();
+	return (0);
+}
+
+//int	render(t_data *data)
+//{
+    ///* if window has been destroyed, we don't want to put the pixel ! */
+    //if (data->win_ptr != NULL)
+        //mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
+            //WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
+    //return (0);
+//}
 
 int main()
 {
-	write(1, "hello", 6);
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (ft_putstr_fd("Error: mlx_init() failed", 2), 1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 300, "GG boiii");
+	data.img = mlx_new_image(data.mlx_ptr, 600, 300);
+	data.img_addr = mlx_get_data_addr(data.img, &data.img_bits_per_pixel, &data.img_line_length,
+			&data.img_endian);
+
+	for (int x = 0; x < 600; x++)
+	{
+		for (int y = 0; y < 300; y++)	
+			my_mlx_pixel_put(&data, x, y, RED);
+	}
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, 0, 0);
+
+	mlx_hook(data.win_ptr, 17, 0, ft_exit, data.mlx_ptr);
+	mlx_hook(data.win_ptr, 2, 0, hook_key, data.mlx_ptr);
+	mlx_loop(data.mlx_ptr);
 	return (0);
 }
