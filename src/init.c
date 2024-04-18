56b5d0f5f244d659fcda04380271f21fbfef5191
PGCL1 <earlybirdcatchestheworm1@gmail.com>
2024-04-18 21:09:35 +0200

    refactor: norminette for init.c

--- src/init.c
+++ src/init.c
@@ -6,25 +6,25 @@
 /*   By: glacroix <glacroix@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 15:13:20 by glacroix          #+#    #+#             */
-/*   Updated: 2024/04/18 17:05:34 by glacroix         ###   ########.fr       */
+/*   Updated: 2024/04/18 21:09:06 by glacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int window_init(t_data *data)
+int	window_init(t_data *data)
 {
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		return (error_msg("mlx_init() failed"), 1);
 	data->win_ptr = mlx_new_window(data->mlx_ptr, w, h, "GG boiii");
 	data->img.img = mlx_new_image(data->mlx_ptr, w, h);
-	data->img.data = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel, &data->img.size_line,
-			&data->img.endian);
+	data->img.data = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
+			&data->img.size_line, &data->img.endian);
 	return (0);
 }
 
-int design_init(t_design *design, t_data *data, int file)
+int	design_init(t_design *design, t_data *data, int file)
 {
 	char	*line;	
 	int		count;
@@ -40,7 +40,7 @@ int design_init(t_design *design, t_data *data, int file)
 	return (0);
 }
 
-int map_init(t_array *map, int file)
+int	map_init(t_array *map, int file)
 {
 	*map = ms_array_init();
 	map_assign(map, file);
@@ -55,14 +55,14 @@ int map_init(t_array *map, int file)
 	return (0);
 }
 
-int player_init(t_player *player, const t_array *map)
+int	player_init(t_player *player, const t_array *map)
 {
 	ft_memset(player, 0, sizeof(player));
 	if (player_start(player, *map) == 1)
 		return (1);
 	if (player->x == 0 && player->y == 0)
 		return (error_msg("the map has to have a player"), 1);
-	return (0);	
+	return (0);
 }
 
 int	setup_init(t_setup *setup, int file)
@@ -87,6 +87,6 @@ int	setup_init(t_setup *setup, int file)
 		return (error_msg("map is not closed"), 6);
 	}
 	free_t_array(&setup->map);
-	setup->map = original;	
-	return (0);	
+	setup->map = original;
+	return (0);
 }
